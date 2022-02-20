826eac2f3a73321c2fc9d15ffc33e1c545eefdee
alex <al3xback@gmail.com>
2022-02-20 11:15:56 +0700

    update config

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git init
 git add -A
 git commit -m 'deploy'
 git branch -m main
-git push -f git@github.com:al3xback/testimonials-vue-FM.git main:gh-pages
+git push -f git@github.com:al3xback/testimonials-vue.git main:gh-pages
 
 cd -
